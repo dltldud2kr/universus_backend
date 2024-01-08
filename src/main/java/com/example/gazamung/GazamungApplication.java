182b892e82537cc8a1878f35dedd9e5fa7adfdbd
dltldud2kr <dltldud2kr@gmail.com>
2024-01-08 16:11:47 +0900

    Initial commit

---
+++ src/main/java/com/example/gazamung/GazamungApplication.java
@@ -0,0 +1,13 @@
+package com.example.gazamung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GazamungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GazamungApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gazamung/GazamungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gazamung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GazamungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
