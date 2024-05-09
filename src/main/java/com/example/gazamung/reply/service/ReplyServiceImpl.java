c5bf1126ff919ec7bf2db58bce3cd6b538639445
winy3ori <winy3ori@gmail.com>
2024-05-09 17:35:23 +0900

    댓글 작성시 글 작성자에게 알림 가도록

--- src/main/java/com/example/gazamung/reply/service/ReplyServiceImpl.java
+++ src/main/java/com/example/gazamung/reply/service/ReplyServiceImpl.java
@@ -1,19 +1,27 @@
 package com.example.gazamung.reply.service;
 
 import com.example.gazamung._enum.CustomExceptionCode;
+import com.example.gazamung._enum.MsgType;
 import com.example.gazamung.exception.CustomException;
+import com.example.gazamung.fcmSend.FcmSendDto;
+import com.example.gazamung.fcmSend.FcmService;
 import com.example.gazamung.member.entity.Member;
 import com.example.gazamung.member.repository.MemberRepository;
+import com.example.gazamung.notification.dto.NotifyCreateReq;
+import com.example.gazamung.notification.service.NotificationService;
+import com.example.gazamung.participant.entity.Participant;
 import com.example.gazamung.reply.dto.CreateDto;
 import com.example.gazamung.reply.dto.InfoDto;
 import com.example.gazamung.reply.dto.ModifyDto;
 import com.example.gazamung.reply.entity.Reply;
 import com.example.gazamung.reply.repository.ReplyRepository;
+import com.example.gazamung.univBoard.entity.UnivBoard;
 import com.example.gazamung.univBoard.repository.UnivBoardRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,15 +34,19 @@ public class ReplyServiceImpl implements ReplyService{
     private final ReplyRepository replyRepository;
     private final MemberRepository memberRepository;
     private final UnivBoardRepository univBoardRepository;
+    private final NotificationService notificationService;
+    private final FcmService fcmService;
 
     @Override
     public Object createReply(CreateDto dto) {
-        memberRepository.findById(dto.getMemberIdx())
-                .orElseThrow(()-> new CustomException(CustomExceptionCode.NOT_FOUND_USER));
+        // 사용자와 게시글 존재 여부를 확인
+        Member member = memberRepository.findById(dto.getMemberIdx())
+                .orElseThrow(() -> new CustomException(CustomExceptionCode.NOT_FOUND_USER));
 
-        univBoardRepository.findById(dto.getUnivBoardId())
-                .orElseThrow(()-> new CustomException(CustomExceptionCode.NOT_FOUND_BOARD));
+        UnivBoard univBoard = univBoardRepository.findById(dto.getUnivBoardId())
+                .orElseThrow(() -> new CustomException(CustomExceptionCode.NOT_FOUND_BOARD));
 
+        // 댓글 생성
         Reply reply = Reply.builder()
                 .memberIdx(dto.getMemberIdx())
                 .univBoardId(dto.getUnivBoardId())
@@ -43,6 +55,23 @@ public class ReplyServiceImpl implements ReplyService{
                 .build();
 
         replyRepository.save(reply);
+
+        // 게시글 작성자에게 알림 보내기
+        if (!member.getMemberIdx().equals(univBoard.getMemberIdx())) { // 자신의 게시글에 댓글을 달 경우 알림을 보내지 않음
+            FcmSendDto fcmSendDto = FcmSendDto.builder()
+                    .token("dWVpAXGoS0-qW8txlowMKt:APA91bEUdfKJYNQYLTDppQVhwQtXoUfwhgYLnTEgoLhZmTXfY8YbK" +
+                            "HeAhiTDoMxXHChr2mhb-eA3eNb0MPUpAHHwceXciW4FZhck-AfWSbHQmwkTHRljIuTFZAhhDYDRKqF2WIZMnpYL")
+                    .title("새로운 댓글이 달렸습니다")
+                    .body(member.getNickname() + "님이 댓글을 작성하셨습니다")
+                    .build();
+            try {
+                fcmService.sendMessageTo(fcmSendDto);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+
 
         return true;
     }
